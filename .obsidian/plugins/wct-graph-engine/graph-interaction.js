d7b101a396af4e9291b96cf4a10618a8bec70088
Richard J. Reyes <reyes.ricky30@gmail.com>
2026-06-27 14:33:06 -0700

    Enable inferred concept navigation in WCT inspector

--- .obsidian/plugins/wct-graph-engine/graph-interaction.js
+++ .obsidian/plugins/wct-graph-engine/graph-interaction.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const input = require("./graph-input");
-const inspector = require("./graph-inspector-v07");
+const inspector = require("./graph-inspector-v071");
 const combined = {};
 for (const source of [input, inspector]) {
   for (const name of Object.getOwnPropertyNames(source)) {
