0b8bfe283bf1e81834d4470a6556efe4a1f90990
Richard J. Reyes <reyes.ricky30@gmail.com>
2026-06-27 03:08:15 -0700

    Combine unified WCT graph interaction modules

---
+++ .obsidian/plugins/wct-graph-engine/graph-interaction.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const input = require("./graph-input");
+const inspector = require("./graph-inspector");
+const combined = {};
+for (const source of [input, inspector]) {
+  for (const name of Object.getOwnPropertyNames(source)) {
+    if (name !== "constructor") {
+      Object.defineProperty(combined, name, Object.getOwnPropertyDescriptor(source, name));
+    }
+  }
+}
+module.exports = combined;
